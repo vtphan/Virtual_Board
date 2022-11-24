98a89af6fafe92af10107c2b23fc143084a37ae6
Lakshmi Saranya Peddi <63470512+lakshmisaranyapeddi76@users.noreply.github.com>
2022-11-24 12:01:26 -0600

    server initial commit

--- virtual_board_server/main.go
+++ virtual_board_server/main.go
@@ -1,102 +1,172 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"virtual_board/virtual_board_server/models"
-
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
+type config struct {
+	Server     string
+	Coursename string
+}
+
 func main() {
+	// EncodeJson() - TODO: Remove this
 	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+	router.Static("/static", "./templates")
+
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/courses", getCourses)
+		v1.GET("/courses", fetchCourses)
+		v1.GET("/courses/view", viewCourses)
+		v1.GET("/courses/:courseName", fetchCourseByName)
 		v1.POST("/courses", addCourse)
 		//v1.PUT("/courses/:courseId", updateCourse)
-		//v1.DELETE("/course/:courseId", deleteCourse)
+		// v1.DELETE("/courses/:courseName", deleteCourse)
 
-		v1.GET("/courses/:courseId/notebooks", viewNotebook)
-		v1.POST("/courses/:courseId/notebooks", addNotebook)
-		v1.PUT("/courses/:courseId/notebooks/:notebookId", updateNotebook)
-		v1.DELETE("/courses/:courseId/notebooks/:notebookId", deleteNotebook)
-
+		v1.GET("/courses/:courseName/notebooks", fetchAllNotebookByCourse)
+		v1.GET("/courses/:courseName/notebooks/view", viewNotebooks)
+		v1.GET("/courses/:courseName/notebooks/:notebookName", fetchNotebookByName)
+		v1.POST("/courses/:courseName/notebooks", addNotebook)
+		v1.PUT("/courses/:courseName/notebooks/:notebookName", updateNotebook)
+		v1.DELETE("/courses/:courseName/notebooks/:notebookName", deleteNotebook)
+		v1.DELETE("/courses/:courseName", deleteCourse)
 	}
 	router.Run()
-
-}
-func checkErr(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
-func getCourses(c *gin.Context) {
+func fetchCourses(c *gin.Context) {
 	courses, err := models.GetCourses()
-	checkErr(err)
-
-	//TODO: Test no courses scenario
-	if courses == nil {
-		c.JSON(http.StatusOK, gin.H{"courses": nil})
-		return
+	if err != nil {
+		logError("fetchCourses: during GetCourses", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	} else {
 		c.JSON(http.StatusOK, gin.H{"courses": courses})
-		return
 	}
 }
 
-func addCourse(c *gin.Context) {
-
-	var json models.Course
-
-	if err := c.ShouldBindJSON(&json); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	success, err := models.AddCourse(json)
-
-	if success {
-		c.JSON(http.StatusCreated, success)
-		return
-	} else {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
-		return
-	}
-}
-
-// func updateCourse(c *gin.Context) {
-// 	c.JSON(http.StatusOK, gin.H{"message": "updateCourse Called"})
-// }
-
-func addNotebook(c *gin.Context) {
-	courseId, err := strconv.Atoi(c.Param("courseId"))
-
-	course_counters, err := models.GetCourseCountById(courseId)
-
-	if course_counters < 1 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Course not found"})
-		return
-	}
-
-	log.Printf("Here 1")
+func fetchCourseByName(c *gin.Context) {
+	courseName := c.Param("courseName")
+	course, err := models.GetCourseByName(courseName)
 	if err != nil {
+		logError("fetchCourseByName: during GetCourseByName", err)
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Course does not exist"})
+			return
+		}
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-	log.Printf("Here 2")
-	var json models.Notebook
+	c.JSON(http.StatusOK, gin.H{"course": course})
+}
 
+func addCourse(c *gin.Context) {
+	var json models.Course
 	if err := c.ShouldBindJSON(&json); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	log.Printf("Here 3")
-	json.CourseId = courseId
+	course, err := models.GetCourseByName(json.Name)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			logError("fetchCourseByName: during GetCourseByName", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	}
 
+	if course != nil {
+		//Course exists
+		c.JSON(http.StatusConflict, gin.H{"error": "Course already exists"})
+		return
+	}
+
+	res, err := models.AddCourse(json)
+
+	if res != nil {
+		id, _ := res.LastInsertId()
+		c.JSON(http.StatusCreated, gin.H{"id": id})
+	} else {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+	}
+}
+
+func fetchAllNotebookByCourse(c *gin.Context) {
+	courseName := c.Param("courseName")
+	course, err := models.GetCourseByName(courseName)
+	if err != nil {
+		logError("fetchAllNotebookByCourse: during GetCourseByName", err)
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Course does not exist"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	notebooks, err := models.GetAllNotebooks(course.Id)
+	if err != nil {
+		logError("fetchAllNotebookByCourse: during GetAllNotebooks", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"notebooks": notebooks})
+}
+
+func fetchNotebookByName(c *gin.Context) {
+	notebookName := c.Param("notebookName")
+	courseName := c.Param("courseName")
+	course, err := models.GetCourseByName(courseName)
+	if err != nil {
+		logError("fetchNotebookByName: during GetCourseByName", err)
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Course does not exist"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	notebook, err := models.GetNotebookByName(course.Id, notebookName)
+
+	if err != nil {
+		logError("fetchNotebookByName: during GetNotebookByName", err)
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Notebook does not exist"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.Writer.WriteHeader(http.StatusOK)
+	c.Writer.Write([]byte(notebook.Content))
+}
+
+func addNotebook(c *gin.Context) {
+	courseName := c.Param("courseName")
+	course, err := models.GetCourseByName(courseName)
+
+	//TODO: revisit for 404
+	if err != nil {
+		logError("addNotebook: during GetCourseByName", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var json models.Notebook
+	if err := c.ShouldBindJSON(&json); err != nil {
+		logError("addNotebook: during ShouldBindJSON", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	json.CourseId = course.Id
 	success, err := models.AddNotebook(json)
 
 	if success {
@@ -106,31 +176,15 @@ func addNotebook(c *gin.Context) {
 	}
 }
 
-func viewNotebook(c *gin.Context) {
-	courseId, err := strconv.Atoi(c.Param("courseId"))
-
-	isWhiteBoards, err := strconv.ParseBool(c.Query("whiteboards"))
-
-	notebook, err := models.ViewNotebook(courseId, isWhiteBoards)
-
-	checkErr(err)
-	// if the name is blank we can assume nothing is found
-	if notebook == nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "No Records Found"})
-		return
-	} else {
-		c.JSON(http.StatusOK, gin.H{"data": notebook})
-	}
-}
-
 func updateNotebook(c *gin.Context) {
-	courseId, err := strconv.Atoi(c.Param("courseId"))
+	notebookName := c.Param("notebookName")
+	courseName := c.Param("courseName")
+	course, err := models.GetCourseByName(courseName)
 
-	notebookId, err := strconv.Atoi(c.Param("notebookId"))
-
-	existingNotebook, err := models.GetNotebookById(courseId, notebookId)
+	existingNotebook, err := models.GetNotebook(course.Id, notebookName)
 
 	if err != nil {
+		fmt.Println("err1", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -145,16 +199,16 @@ func updateNotebook(c *gin.Context) {
 	if success {
 		c.Status(http.StatusNoContent)
 	} else {
+		fmt.Println("err2", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
-
 }
 
 func deleteNotebook(c *gin.Context) {
 
-	cn := c.Param("coursename")
-
-	success, err := models.DeleteNotebook(cn)
+	cn := c.Param("courseName")
+	course, err := models.GetCourseByName(cn)
+	success, err := models.DeleteNotebook(course.Id)
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
@@ -165,4 +219,96 @@ func deleteNotebook(c *gin.Context) {
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err})
 	}
+}
+func deleteCourse(c *gin.Context) {
+
+	cn := c.Param("courseName")
+	success, err := models.DeleteCourse(cn)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+	}
+
+	if success {
+		c.JSON(http.StatusOK, gin.H{"message": "Success"})
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+	}
+}
+
+func logError(message string, err error) {
+	log.Print("At: {} \n Error: {}", message, err)
+}
+
+func viewCourses(c *gin.Context) {
+	baseUrl := "http://localhost:8080/api/v1/courses/"
+	courses, err := models.GetCourses()
+	if err != nil {
+		logError("fetchCourses: during GetCourses", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	courseNames := populateCourseNames(courses)
+	c.HTML(http.StatusOK, "course.html", gin.H{
+		"title":       "Displays courses",
+		"baseUrl":     baseUrl,
+		"courseNames": courseNames,
+	})
+}
+
+func viewNotebooks(c *gin.Context) {
+	courseName := c.Param("courseName")
+	baseUrl := "http://localhost:8080/api/v1/courses/" + courseName + "/notebooks/"
+	course, err := models.GetCourseByName(courseName)
+	if err != nil {
+		logError("fetchAllNotebookByCourse: during GetCourseByName", err)
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Course does not exist"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	notebooks, err := models.GetAllNotebooks(course.Id)
+
+	notebook := populateNotebookNames(notebooks)
+	c.HTML(http.StatusOK, "notebook.html", gin.H{
+		"title":     "Displays notebooks",
+		"baseUrl":   baseUrl,
+		"notebooks": notebook,
+		"course":    courseToString(course),
+	})
+}
+
+func populateNotebookNames(notebooks []models.Notebook) string {
+	s := make([]string, 0, len(notebooks))
+	for _, notebook := range notebooks {
+		bytes, err := json.Marshal(notebook)
+		if err != nil {
+			panic(err)
+		}
+		s = append(s, string(bytes))
+	}
+	output := strings.Join(s, `&`)
+	return output
+}
+
+func populateCourseNames(courses []models.Course) string {
+	s := make([]string, 0, len(courses))
+	for _, course := range courses {
+		bytes, err := json.Marshal(course)
+		if err != nil {
+			panic(err)
+		}
+		s = append(s, string(bytes))
+	}
+	output := strings.Join(s, `&`)
+	return output
+}
+
+func courseToString(course *models.Course) string {
+	bytes, err := json.Marshal(course)
+	if err != nil {
+		panic(err)
+	}
+	return string(bytes)
 }
